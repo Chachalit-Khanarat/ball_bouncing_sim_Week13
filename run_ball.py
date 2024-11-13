d2fa21b4efe48f9bcf95865037e7fbd9d426de76
Chachalit Khanarat <Chachalit.k@ku.th>
2024-11-13 09:48:46 +0700

    make the code runable

--- run_ball.py
+++ run_ball.py
@@ -8,6 +8,7 @@ turtle.tracer(0)
 turtle.hideturtle()
 canvas_width = turtle.screensize()[0]
 canvas_height = turtle.screensize()[1]
+print(turtle.screensize())
 print(canvas_width, canvas_height)
 ball_radius = 0.05 * canvas_width
 turtle.colormode(255)
@@ -20,8 +21,8 @@ ball_color = []
 
 # create random number of balls, num_balls, located at random positions within the canvas; each ball has a random velocity value in the x and y direction and is painted with a random color
 for i in range(num_balls):
-    xpos.append(random.randint(-1*canvas_width + ball_radius, canvas_width - ball_radius))
-    ypos.append(random.randint(-1*canvas_height + ball_radius, canvas_height - ball_radius))
+    xpos.append(random.randint(int(-1*canvas_width + ball_radius), int(canvas_width - ball_radius)))
+    ypos.append(random.randint(int(-1*canvas_height + ball_radius), int(canvas_height - ball_radius)))
     vx.append(2*random.uniform(-1.0, 1.0))
     vy.append(2*random.uniform(-1.0, 1.0))
     ball_color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
